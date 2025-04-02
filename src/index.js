c9b06f7d00cf3d919518707d29cf7f176be639c1
Jaco-orgel <OrgelPCJaco@hotmail.com>
2025-04-02 10:03:28 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import KrantenwijkApp from "./App"; // Zorg ervoor dat dit overeenkomt met de naam van je component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <KrantenwijkApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
